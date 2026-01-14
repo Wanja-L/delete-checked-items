13b989fdd0c821493e738da7fc69a6f72193e5cf
Wanja Leuthold <28242442+Wanja-L@users.noreply.github.com>
2026-01-14 10:03:08 +0100

    Add command and file menu item for deleting checked items

--- src/main.ts
+++ src/main.ts
@@ -1,99 +1,66 @@
-import {App, Editor, MarkdownView, Modal, Notice, Plugin} from 'obsidian';
-import {DEFAULT_SETTINGS, MyPluginSettings, SampleSettingTab} from "./settings";
+import { Editor, MarkdownView, Plugin } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class DeleteCheckedItems extends Plugin {
 
 	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		this.addRibbonIcon('dice', 'Sample', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status bar text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-modal-simple',
-			name: 'Open modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
 		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
-			id: 'replace-selected',
-			name: 'Replace selected content',
+			id: 'run',
+			name: 'Run',
 			editorCallback: (editor: Editor, view: MarkdownView) => {
-				editor.replaceSelection('Sample editor command');
+				this.deleteCheckedItems(editor)
 			}
 		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-modal-complex',
-			name: 'Open modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+		this.registerEvent(
+			this.app.workspace.on("file-menu", (menu, _editor, view) => {
+				const activeMDView = this.app.workspace.getActiveViewOfType(MarkdownView);
+
+				// only make this option available in markdown files
+				if (activeMDView) {
+					menu.addItem((item) => {
+						item
+							.setTitle('Delete checked items')
+							.setIcon('square-x')
+							.onClick(async () => {
+								this.deleteCheckedItems(activeMDView.editor)
+							});
+					});
 				}
-				return false;
-			}
-		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			new Notice("Click");
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-
+			})
+		);
 	}
 
-	onunload() {
+	deleteCheckedItems(editor: Editor) {
+		// select all because Editor.processLines only operates on the current selection
+		editor.setSelection({ ch: 0, line: 0 }, { ch: 0, line: editor.lineCount() })
+
+		editor.processLines(
+			// Check if line contains a checked item
+			(_lineNum, lineText) => {
+				return !!lineText.match(/\s*- \[x\].*/)
+			},
+			// Delete matched lines
+			(lineNum, _lineText, shouldDelete) => {
+				if (shouldDelete) {
+					// Return empty text to delete the line
+					return {
+						from: { line: lineNum, ch: 0 },
+						to: { line: lineNum + 1, ch: 0 },  // Include newline
+						text: ''
+					};
+				}
+				return undefined;  // Keep line unchanged
+			},
+			true
+		);
+
+		// reset cursor to cancel the selection
+		editor.setCursor(editor.lineCount())
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData() as Partial<MyPluginSettings>);
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		let {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
 }
